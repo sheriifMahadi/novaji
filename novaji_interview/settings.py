d1c117e78d289bc3588df6640b1ee49a49aac8dd
sheriifMahadi <sheriffabdulamjid@gmail.com>
2024-10-28 13:15:36 +0100

    requirements

--- novaji_interview/settings.py
+++ novaji_interview/settings.py
@@ -39,9 +39,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'online_registration_form',
     'crispy_forms',
+    "crispy_bootstrap4"
 
 ]
 
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
